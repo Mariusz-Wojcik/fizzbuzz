97e9c961e2b3daa9e38b0219fae41450710ba597
Mariusz Wójcik <36dragan@gmail.com>
2020-08-01 23:19:09 +0200

    move logic to FuzzBuzz class and add exception verification

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -8,27 +8,28 @@ namespace fizzbuzz
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 10; i++)
+         static void Main(string[] args)
+         {
+            try
             {
-                Console.WriteLine("Podaj liczbę:");
-                var userNumber = int.Parse(Console.ReadLine());
-
-                if (userNumber % 3 == 0)
-                {
-                    if (userNumber % 5 == 0)
-                        Console.WriteLine("FIZZBUZZ");
-                    else
-                        Console.WriteLine("FIZZ");
-                }
-                else if (userNumber % 5 == 0)
-                {
-                    Console.WriteLine("BUZZ");
-                }
-                else
-                    Console.WriteLine(userNumber);
+            Console.WriteLine("Podaj liczbę:");
+            var userNumber = GetData();
+            
+            var FizzBuzz = new FizzBuzz(userNumber);
+            FizzBuzz.ToString();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static int GetData()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int data))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+            }
+            return data;
         }
     }
 }
--- fizzbuzz/fizzbuzz.cs
+++ fizzbuzz/fizzbuzz.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace fizzbuzz
 {
-    class fizzbuzz
+    class FizzBuzz
     {
+        public FizzBuzz(int a)
+        {
+            if (a % 3 == 0)
+            {
+                if (a % 5 == 0)
+                    Console.WriteLine("FIZZBUZZ");
+                else
+                    Console.WriteLine("FIZZ");
+            }
+            else if (a % 5 == 0)
+            {
+                Console.WriteLine("BUZZ");
+            }
+            else
+                Console.WriteLine(a);
+        }
     }
 }
